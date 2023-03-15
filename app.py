910248cab44de590addc62175962c74bade90d80
GehanPatil <122773752+GehanPatil@users.noreply.github.com>
2023-03-15 15:17:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,35 +3,34 @@ import streamlit as st
 import pickle
 import numpy as np
 
-st.set_page_config(page_title="Laptop Price Predictor", page_icon="💻",
-                   layout="wide")
-
 #import model
 st.title("Laptop Price Predictor 💻")
 pipe=pickle.load(open("pipe.pkl","rb"))
 df=pickle.load(open("df.pkl","rb"))
 
-# making 3 cols left_column, middle_column, right_column
-left_column, middle_column, right_column = st.columns(3)
+# making 2 cols left_column, right_column
+left_column, right_column = st.columns(2)
 with left_column:
     # brand input
     company = st.selectbox("Brand", df["Company"].unique())
 
-with middle_column:
+with right_column:
     # laptop type
     type = st.selectbox("Type", df["TypeName"].unique())
 
-with right_column:
+# making 2 cols left_column, right_column
+left_column, right_column = st.columns(2)
+with left_column:
     # Ram size
     ram = st.selectbox("Ram (in GB)", df["Ram"].unique())
 
-# making 3 cols left_column, middle_column, right_column
-left_column, middle_column, right_column = st.columns(3)
-with left_column:
+with right_column:
     # Weight input
     weight = st.number_input("Weight of laptop in kg")
 
-with middle_column:
+# making 2 cols left_column, right_column
+left_column, right_column = st.columns(2)
+with left_column:
     # Touchscreen
     touchscreen = st.selectbox("Touchscreen", ["No", "Yes"])
 
@@ -39,21 +38,18 @@ with right_column:
     # IPS display
     ips = st.selectbox("IPS Display", ["No", "Yes"])
 
-# making 3 cols left_column, middle_column, right_column
-left_column, middle_column, right_column = st.columns(3)
+# making 2 cols left_column, right_column
+left_column, right_column = st.columns(2)
 with left_column:
     # screen size
-    Screen_size = st.number_input("Screen Size (in Inches)")
+    Screen_size = st.selectbox("Screen Size (in Inches)", df["Inches"].unique())
 
-with middle_column:
+with right_column:
     # resolution
   resolution = st.selectbox('Screen Resolution',['1920x1080', '1366x768', '1600x900', '3840x2160', '3200x1800', '2880x1800', '2560x1600','2560x1440', '2304x1440'])
-with right_column:
-    # cpu input
-    cpu = st.selectbox("CPU Brand", df["Cpu brand"].unique())
 
-# making 3 cols left_column, middle_column, right_column
-left_column,  right_column = st.columns(2)
+# making 2 cols left_column, right_column
+left_column, right_column = st.columns(2)
 with left_column:
     # hdd input
     hdd = st.selectbox("HDD(in GB)", [0, 128, 256, 512, 1024, 2048])
@@ -63,6 +59,9 @@ with right_column:
     # ssd input
     ssd = st.selectbox("SSD(in GB)", [0, 8, 128, 256, 512, 1024])
 
+# cpu input
+cpu = st.selectbox("CPU Brand", df["Cpu brand"].unique())    
+    
 #gpu input
 gpu=st.selectbox("GPU Brand",df["Gpu brand"].unique())
 
