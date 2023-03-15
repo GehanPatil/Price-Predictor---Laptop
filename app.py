5fd89db41a348a48b2d2efe78e07f11dd243401f
GehanPatil <122773752+GehanPatil@users.noreply.github.com>
2023-03-15 15:08:13 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ import streamlit as st
 import pickle
 import numpy as np
 
+st.set_page_config(page_title="Laptop Price Predictor", page_icon="💻",
+                   layout="wide")
 
 #import model
 st.title("Laptop Price Predictor 💻")
 pipe=pickle.load(open("pipe.pkl","rb"))
 df=pickle.load(open("df.pkl","rb"))
 
-# Line 1 - making 3 cols left_column, middle_column, right_column
+# making 3 cols left_column, middle_column, right_column
 left_column, middle_column, right_column = st.columns(3)
 with left_column:
     # brand input
@@ -23,7 +25,21 @@ with right_column:
     # Ram size
     ram = st.selectbox("Ram (in GB)", df["Ram"].unique())
 
-# Line 2 - making 3 cols left_column, middle_column, right_column
+# making 3 cols left_column, middle_column, right_column
+left_column, middle_column, right_column = st.columns(3)
+with left_column:
+    # Weight input
+    weight = st.number_input("Weight of laptop in kg")
+
+with middle_column:
+    # Touchscreen
+    touchscreen = st.selectbox("Touchscreen", ["No", "Yes"])
+
+with right_column:
+    # IPS display
+    ips = st.selectbox("IPS Display", ["No", "Yes"])
+
+# making 3 cols left_column, middle_column, right_column
 left_column, middle_column, right_column = st.columns(3)
 with left_column:
     # screen size
@@ -35,36 +51,23 @@ with middle_column:
 with right_column:
     # cpu input
     cpu = st.selectbox("CPU Brand", df["Cpu brand"].unique())
-    
-# Line 3 - making 2 cols left_column, right_column
-left_column, right_column = st.columns(2)
-with left_column:
-    # Touchscreen
-    touchscreen = st.selectbox("Touchscreen", ["No", "Yes"])
 
-with right_column:
-    # IPS display
-    ips = st.selectbox("IPS Display", ["No", "Yes"])
-
-# Line 4 - making 2 cols left_column, right_column
+# making 3 cols left_column, middle_column, right_column
 left_column,  right_column = st.columns(2)
 with left_column:
     # hdd input
     hdd = st.selectbox("HDD(in GB)", [0, 128, 256, 512, 1024, 2048])
 
+
 with right_column:
     # ssd input
     ssd = st.selectbox("SSD(in GB)", [0, 8, 128, 256, 512, 1024])
 
-# Line 5 - making 2 cols left_column, right_column
-left_column,  right_column = st.columns(2)
-with left_column:
-    #gpu input
-    gpu=st.selectbox("GPU Brand",df["Gpu brand"].unique())
+#gpu input
+gpu=st.selectbox("GPU Brand",df["Gpu brand"].unique())
 
-with right_column:
-    #os input
-    os=st.selectbox("OS Type",df["os"].unique())
+#os input
+os=st.selectbox("OS Type",df["os"].unique())
 
 if st.button("Pridict Price"):
     ppi = None
@@ -81,9 +84,9 @@ if st.button("Pridict Price"):
     X_res=int(resolution.split("x")[0])
     Y_res=int(resolution.split('x')[1])
     ppi=((X_res ** 2)+(Y_res ** 2))**0.5/Screen_size
-    query=np.array([company, type, ram, touchscreen, ips, ppi, cpu, hdd, ssd, gpu, os])
+    query=np.array([company, type, ram, weight, touchscreen, ips, ppi, cpu, hdd, ssd, gpu, os])
 
-    query=query.reshape(1, 11)
+    query=query.reshape(1, 12)
     st.title("The Predicted Price of Laptop = Rs "+str(int(np.exp(pipe.predict(query)[0]))))
 
 
